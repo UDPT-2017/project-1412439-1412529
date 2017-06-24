c60d7d166141be1d569b6d70bf364a3870c7621d
NguyenHoangQuan2503 <nhquan.fit.hcmus@gmail.com>
2017-06-24 09:19:01 +0700

    change somthing

--- db.js
+++ db.js
@@ -1,33 +1,36 @@
-const pg=require("./app/models/connectDB");//goi bien pg ra de ket noi db,bien nay o thu muc models
 
-pg.connect(function(err, client,done) {//ket noi db.cac cau lenh truy van duoc dat trong ham connect
-      if(err){
 
-         console.log(err);
-       }
-       else{//ket noi thanh cong
-        console.log("connect to postgres");
+const pg = require("./app/models/connectDB");//goi bien pg ra de ket noi db,bien nay o thu muc models
+
+pg.connect(function (err, client, done) {//ket noi db.cac cau lenh truy van duoc dat trong ham connect
+  if (err) {
+    console.log(err);
+  }
+  else {//ket noi thanh cong
+    console.log("connect to postgres success!");
     //    var query="update messages set message='{"+'"'+req.body.data+'","'+req.body.time1+'","'+req.body.time2+'","'+req.body.status+'"'+"}'"+" where user1='"+req.body.user+"' and user2='"+req.session.user+"' and message[2]='"+req.body.time1+"'";
-        var query='select * from admin';
-        console.log(query);
-          client.query(query, function (err, result) {//truy van,neu co ket qua se tra ve trong bien result
-            done();
-          if(err){
+    
+     var query='ALTER TABLE ONLY transaction ADD CONSTRAINT transaction_user_pkey FOREIGN KEY (user_id) REFERENCES client(id);';
 
-            console.log(err);
+      console.log(query);
+    client.query(query, function (err, result) {//truy van,neu co ket qua se tra ve trong b ien result
+      done();
+      if (err) {
 
-
-          }
-          else{
-
-            for(var i=0;i<result.rows.length;i++){
-              console.log(result.rows[i]);
-            }
-
-          }
-        });
+        console.log(err);
 
 
-    }
+      }
+      else {
 
-     });
+        for (var i = 0; i < result.rows.length; i++) {
+          console.log(result.rows[i]);
+        }
+
+      }
+    });
+
+
+  }
+
+});
