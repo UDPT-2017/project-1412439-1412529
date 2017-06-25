171279fb210d7190be946535f9e10a65051b4992
NguyenHoangQuan2503 <nhquan.fit.hcmus@gmail.com>
2017-06-25 15:47:24 +0700

    make it better

--- index.js
+++ index.js
@@ -2,7 +2,4 @@ const pg=require("./app/models/connectDB");
 var express = require('express');
 var app = express();
 require('./config')(app);
-var port = 3000;
-app.listen(port, function () {
-  console.log('Example app listening on port ' + port + '!');
-});
+app.listen(process.env.PORT || 3000);
